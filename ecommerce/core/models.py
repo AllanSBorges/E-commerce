93b4178182a68fdb024299a8035ad13e54319fd2
AllanSBorges <allandasilvaborges33@gmail.com>
2023-11-10 09:30:55 -0300

    feat: Criação do Views do app Core

--- ecommerce/core/models.py
+++ ecommerce/core/models.py
@@ -6,6 +6,17 @@ class Products(models.Model):
     product_name = models.CharField(max_length = 30)
     product_description = models.CharField(max_length = 60)
     product_price = models.DecimalField(max_digits=7, decimal_places=2)
-    product_status = models.Boolean()
+    product_status = models.BooleanField()
+
+class Categories(models.Model):
+    category_name = models.CharField(max_length = 30)
+    category_description = models.CharField(max_length = 60)
+    
+class ProductCategories(models.Model):
+    product_id = models.ForeignKey("Products", on_delete=models.CASCADE)
+    category_id = models.ForeignKey("Categories", on_delete=models.CASCADE)
+    
+
+    
     
     
--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def page(request):
+    html = "<h1>Hello World</h1>"
+    return HttpResponse(html)
+
+def dois(request):
+    html = "<h1>2</h1>"
+    return HttpResponse(html)
