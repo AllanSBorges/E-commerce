1bd9c6047f3e167f592186ccf76c24fbc32c6c94
AllanSBorges <allandasilvaborges33@gmail.com>
2023-11-15 11:06:37 -0300

    feat: Configuração do Bootstrap

--- ecommerce/core/urls.py
+++ ecommerce/core/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.page, name="page"),
+    path("index.html", views.index, name="index"),
+    path("page/<int:year>/<int:month>", views.dois, name="dois"),
+    path("single-product.html", views.product, name="single-product"),
     path("page", views.dois, name="dois"),
+    path("now", views.time_now, name="time"),
 ]
 
--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,10 +1,29 @@
 from django.http import HttpResponse
+from django.template import loader
+import datetime
 # Create your views here.
 
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
 def page(request):
-    html = "<h1>Hello World</h1>"
+    template = loader.get_template('index.html')
+    context = {"nome": "Allan",
+               "data_nasc": "22/07/1991", }    
+    return HttpResponse(template.render(context))
+
+def product(request):
+    template = loader.get_template('single-product.html')
+    context = {"nome": "Allan",
+               "data_nasc": "22/07/1991", }    
+    return HttpResponse(template.render(context))
+
+def dois(request,year=2000,month=1):
+    html = "<h1>",year,"</h1>","<h2>",month,"</h2>"
     return HttpResponse(html)
 
-def dois(request):
-    html = "<h1>2</h1>"
+def time_now(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
