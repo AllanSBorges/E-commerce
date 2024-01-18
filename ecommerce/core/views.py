3575f0a0cabb4dead8024b8cb6c960a7ad5e0a13
AllanSBorges <allandasilvaborges33@gmail.com>
2024-01-18 16:10:32 -0300

    feat: Criação da Paginação dos Pordutos.

--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.http import JsonResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from random import sample
 import datetime
 
@@ -14,30 +15,32 @@ import datetime
 
 
 def login_view(request):
-    
-	if request.method == 'POST':
-		username = request.POST['username']
-		password = request.POST['password']
+    categorias = Category.objects.all()
+    context = {'categorias': categorias}
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 		# Authenticate
-		user = authenticate(request, username=username, password=password)
-		if user is not None:
-			login(request, user)
-			messages.success(request, "You Have Been Logged In!")
-			return redirect('login')
-		else:
-			messages.success(request, "There Was An Error Logging In, Please Try Again...")
-			return redirect('login')
-	else:
-		return render(request, 'login.html', {})
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged In!")
+            return redirect('login')
+        else:
+            messages.success(request, "There Was An Error Logging In, Please Try Again...")
+            return redirect('login')
+    else:
+        return render(request, 'login.html', context)
 
     
 def logout_view(request):
     logout(request)
-    return redirect('index')
+    return redirect('page')
     
 
 def signup_view(request):
-    context = {}
+    categorias = Category.objects.all()
+    context = {'categorias': categorias}
     return render(request, 'signup.html',context)
 
 
@@ -59,15 +62,18 @@ def index_view(request):
             lista.append((categoria,produtos_da_categoria))
     
     context= {'lista': lista,
-              'escolhidas': escolhidas}
+              'escolhidas': escolhidas,
+              'categorias': categorias}
         
         
-    print(context)
+    
     return render(request, 'index.html', context )
 
 def product_view(request,pk):
+    categorias = Category.objects.all()
     prod = Product.objects.get(pk=pk)
-    context = {"product": prod }    
+    context = {"product": prod,
+               'categorias': categorias }    
     return render(request,'single-product.html',context)
 
 def dois(request,year=2000,month=1):
@@ -82,7 +88,7 @@ def time_now(request):
 
 def order_view(request):
      # Obtém os dados do cookie "cart" ou uma lista vazia se o cookie não existir
-    
+    categorias = Category.objects.all()
     
     if request.method == 'POST':
         produto = request.POST['product_name']
@@ -111,7 +117,8 @@ def order_view(request):
         request.session['cart'] = carrinho
 
     pedido = request.session.get('cart', {})
-    context = {'pedido' : pedido}
+    context = {'pedido' : pedido,
+               'categorias' :categorias, }
     template = loader.get_template('order.html')
     return render(request,'order.html', context)
 
@@ -120,8 +127,23 @@ def category_view(request, pk):
     categoria = Category.objects.get(pk=pk)
     produtos_da_categoria = categoria.productcategory_set.all()
     
+    # Configurar a paginação
+    paginator = Paginator(produtos_da_categoria, 1)  # 12 itens por página
+    page = request.GET.get('page')
+
+    try:
+        itens_paginados = paginator.page(page)
+    except PageNotAnInteger:
+        # Se a página não é um número inteiro, exibir a primeira página
+        itens_paginados = paginator.page(1)
+    except EmptyPage:
+        # Se a página está fora do intervalo (e.g., 9999), exibir a última página
+        itens_paginados = paginator.page(paginator.num_pages)
+
+
     context = {'categoria': categoria,
-               'produtos': produtos_da_categoria}
+               'produtos': produtos_da_categoria,
+               'itens_paginados': itens_paginados}
     print(context)
     return render(request,'products.html', context)
 
